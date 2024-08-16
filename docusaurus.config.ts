55866eead7126d337431f773501af26e89dc27b9
ame0011 <he40@neko2.net>
2024-08-16 23:20:00 +0900

    remark-breaskを追加

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,6 +39,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          remarkPlugins: [require('remark-breaks')],
         },
         blog: {
           showReadingTime: true,
@@ -46,6 +47,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          remarkPlugins: [require('remark-breaks')],
         },
         theme: {
           customCss: './src/css/custom.css',
